be804c7dcf6651efa1491bf0f6fb004701f64889
thomassth <thomassth@users.noreply.github.com>
2025-06-16 02:05:05 -0400

    design tweaks

--- client/src/app/core/dashboard.tsx
+++ client/src/app/core/dashboard.tsx
@@ -2,9 +2,14 @@ import CoreBase from './base';
 
 const DashboardPage = () => (
   <CoreBase>
-    <div className='flex flex-col items-start justify-center h-full p-10'>
-      <h1 className='text-5xl font-bold mb-8'>Welcome to Convergent!</h1>
+      <div className='flex flex-col items-start h-full p-10'>
+        <div id="heading" className='flex w-full justify-between'>
+          <div id="hdeading-text">
+            <h1 className='text-5xl font-bold mb-8'>Conversations</h1>
       <h2>Start or participate in a conversation.</h2>
+          </div>
+          <a className='mb-8 p-2 bg-secondary text-white rounded-md h-fit' href='/conversation/new'>+ Create conversation</a>
+        </div>
     </div>
   </CoreBase>
 );
--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -1,11 +1,11 @@
-const Logo = ({ fill }: { fill: string }) => (
+const Logo = ({ fill }: { fill?: string }) => (
   <svg
     width="20"
     viewBox="0 0 88 100"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M44 0L87.3013 25V75L44 100L0.69873 75V25L44 0Z" fill={fill}></path>
+    <path d="M44 0L87.3013 25V75L44 100L0.69873 75V25L44 0Z" fill={fill ?? 'black'}></path>
   </svg>
 );
 
