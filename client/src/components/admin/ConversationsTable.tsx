f42891d858139dc9cb05cfb86f628d87cf08dbc4
Maanas Arora <maanasarora23@gmail.com>
2025-06-23 20:35:08 -0400

    ENH: Improve conversation page UI in accordance with UI designs

--- client/src/components/admin/ConversationsTable.tsx
+++ client/src/components/admin/ConversationsTable.tsx
@@ -1,38 +1,71 @@
-import { Conversation } from "../../app/core/dashboard"
+import { Input } from '@headlessui/react';
+import { Conversation } from '../../app/core/dashboard';
 
-export const ConversationTable = ({ conversations }: { conversations: Conversation[] }) => {
-    return (
-        <table id="conversation-table">
-            <thead>
-                <tr className="grid grid-cols-[1fr_1fr_1fr_0.5fr]">
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {conversations.map(conversation => (
-                    <ConversationTableItem conversation={conversation} />
-                ))}
-            </tbody>
-        </table>
-    )
-}
-
-const ConversationTableItem = ({ conversation }: { conversation: Conversation }) => {
-    const viewLink = `/conversation/${conversation.id}`
-    const editLink = `/conversation/${conversation.id}/edit`
-
-    return (
-        <tr className="grid grid-cols-[1fr_1fr_1fr_0.5fr] border-b-2 border-gray-400">
-            <th>{conversation.id}</th>
-            <th>{conversation.name}</th>
-            <th>{conversation.description}</th>
-            <th className="flex gap-1 place-content-center">
-                <a href={viewLink}>View</a>
-                <a href={editLink}>Edit</a>
+export const ConversationTable = ({
+  conversations,
+}: {
+  conversations: Conversation[];
+}) => {
+  return (
+    <div className='border border-gray-200 rounded-2xl shadow-lg w-full'>
+      <table id='conversation-table' className='w-full border-collapse'>
+        <thead>
+          <tr className='grid grid-cols-[0.2fr_2fr_1fr_1fr_0.5fr_1fr_0.5fr] text-gray-500 items-center bottom-2 border-b-gray-200 border-b'>
+            <th className='text-left p-5'>
+              <Input type='checkbox' className='h-5 w-5' />
             </th>
-        </tr>
-    )
-}
+            <th className='text-left p-5'>Conversation Title</th>
+            <th className='text-left p-5'>Number of Participants</th>
+            <th className='text-left p-5'>Date Created</th>
+            <th className='text-left p-5'>Status</th>
+            <th className='text-left p-5'>Created By</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {conversations.map((conversation) => (
+            <ConversationTableItem conversation={conversation} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const ConversationTableItem = ({
+  conversation,
+}: {
+  conversation: Conversation;
+}) => {
+  const viewLink = `/conversation/${conversation.id}`;
+  const editLink = `/conversation/${conversation.id}/edit`;
+
+  return (
+    <tr className='grid grid-cols-[0.2fr_2fr_1fr_1fr_0.5fr_1fr_0.5fr] items-center'>
+      <td className='p-5'>
+        <Input type='checkbox' className='h-5 w-5' />
+      </td>
+      <td className='p-5'>
+        <strong>{conversation.name}</strong>
+        <br />
+        {conversation.description}
+      </td>
+      <td />
+      <td />
+      <td />
+      <td className='p-5'>
+        <span className='text-gray-600'>
+          {conversation.author.username}
+        </span>
+      </td>
+      <td className='p-5 flex gap-4'>
+        <a href={viewLink} className='text-gray-800 hover:underline'>
+          View
+        </a>
+        <a href={editLink} className='text-gray-800 hover:underline'>
+          Edit
+        </a>
+      </td>
+    </tr>
+  );
+};
