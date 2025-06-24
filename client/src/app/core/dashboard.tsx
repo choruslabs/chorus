7b9f83cfb9fb22208cbb0b301e7a0966cef65e0a
Maanas Arora <maanasarora23@gmail.com>
2025-06-23 21:33:25 -0400

    MAINT: Prettify with Prettier

--- client/src/app/core/dashboard.tsx
+++ client/src/app/core/dashboard.tsx
@@ -1,30 +1,42 @@
-import { useQuery } from '@tanstack/react-query';
-import CoreBase from './base';
-import { getApi } from '../../components/api/base';
-import { ConversationTable } from '../../components/admin/ConversationsTable';
+import { useQuery } from "@tanstack/react-query";
+import CoreBase from "./base";
+import { getApi } from "../../components/api/base";
+import { ConversationTable } from "../../components/admin/ConversationsTable";
 
-export type Conversation = { id: string, name: string, description: string, author: { username: string } };
+export type Conversation = {
+  id: string;
+  name: string;
+  description: string;
+  author: { username: string };
+};
 
 const DashboardPage = () => {
-
   const conversations = useQuery<Conversation[]>({
-    queryKey: ['conversations'], queryFn: () => getApi('/conversations')
-  })
+    queryKey: ["conversations"],
+    queryFn: () => getApi("/conversations"),
+  });
 
   return (
     <CoreBase>
-      <div className='flex flex-col items-start h-full w-[90%] mx-auto py-10'>
-        <div id="heading" className='flex w-full justify-between flex-wrap'>
+      <div className="flex flex-col items-start h-full w-[90%] mx-auto py-10">
+        <div id="heading" className="flex w-full justify-between flex-wrap">
           <div id="hdeading-text">
-            <h1 className='text-5xl font-bold mb-8'>Conversations</h1>
-            <h2 className='mb-16'>Find all conversations created below.</h2>
+            <h1 className="text-5xl font-bold mb-8">Conversations</h1>
+            <h2 className="mb-16">Find all conversations created below.</h2>
           </div>
-          <a className='mb-8 px-5 py-3 bg-secondary text-white rounded-md h-fit' href='/conversation/new'>+ Create conversation</a>
+          <a
+            className="mb-8 px-5 py-3 bg-secondary text-white rounded-md h-fit"
+            href="/conversation/new"
+          >
+            + Create conversation
+          </a>
         </div>
-        {conversations.data && <ConversationTable conversations={conversations.data} />}
+        {conversations.data && (
+          <ConversationTable conversations={conversations.data} />
+        )}
       </div>
     </CoreBase>
-  )
+  );
 };
 
 export default DashboardPage;
--- client/src/components/admin/ConversationsTable.tsx
+++ client/src/components/admin/ConversationsTable.tsx
@@ -1,5 +1,5 @@
-import { Input } from '@headlessui/react';
-import { Conversation } from '../../app/core/dashboard';
+import { Input } from "@headlessui/react";
+import { Conversation } from "../../app/core/dashboard";
 
 export const ConversationTable = ({
   conversations,
@@ -7,18 +7,18 @@ export const ConversationTable = ({
   conversations: Conversation[];
 }) => {
   return (
-    <div className='border border-gray-200 rounded-2xl shadow-lg w-full'>
-      <table id='conversation-table' className='w-full border-collapse'>
+    <div className="border border-gray-200 rounded-2xl shadow-lg w-full">
+      <table id="conversation-table" className="w-full border-collapse">
         <thead>
-          <tr className='grid grid-cols-[0.2fr_2fr_1fr_1fr_0.5fr_1fr_0.5fr] text-gray-500 items-center bottom-2 border-b-gray-200 border-b'>
-            <th className='text-left p-5'>
-              <Input type='checkbox' className='h-5 w-5' />
+          <tr className="grid grid-cols-[0.2fr_2fr_1fr_1fr_0.5fr_1fr_0.5fr] text-gray-500 items-center bottom-2 border-b-gray-200 border-b">
+            <th className="text-left p-5">
+              <Input type="checkbox" className="h-5 w-5" />
             </th>
-            <th className='text-left p-5'>Conversation Title</th>
-            <th className='text-left p-5'>Number of Participants</th>
-            <th className='text-left p-5'>Date Created</th>
-            <th className='text-left p-5'>Status</th>
-            <th className='text-left p-5'>Created By</th>
+            <th className="text-left p-5">Conversation Title</th>
+            <th className="text-left p-5">Number of Participants</th>
+            <th className="text-left p-5">Date Created</th>
+            <th className="text-left p-5">Status</th>
+            <th className="text-left p-5">Created By</th>
             <th></th>
           </tr>
         </thead>
@@ -41,11 +41,11 @@ const ConversationTableItem = ({
   const editLink = `/conversation/${conversation.id}/edit`;
 
   return (
-    <tr className='grid grid-cols-[0.2fr_2fr_1fr_1fr_0.5fr_1fr_0.5fr] items-center'>
-      <td className='p-5'>
-        <Input type='checkbox' className='h-5 w-5' />
+    <tr className="grid grid-cols-[0.2fr_2fr_1fr_1fr_0.5fr_1fr_0.5fr] items-center">
+      <td className="p-5">
+        <Input type="checkbox" className="h-5 w-5" />
       </td>
-      <td className='p-5'>
+      <td className="p-5">
         <strong>{conversation.name}</strong>
         <br />
         {conversation.description}
@@ -53,16 +53,14 @@ const ConversationTableItem = ({
       <td />
       <td />
       <td />
-      <td className='p-5'>
-        <span className='text-gray-600'>
-          {conversation.author.username}
-        </span>
+      <td className="p-5">
+        <span className="text-gray-600">{conversation.author.username}</span>
       </td>
-      <td className='p-5 flex gap-4'>
-        <a href={viewLink} className='text-gray-800 hover:underline'>
+      <td className="p-5 flex gap-4">
+        <a href={viewLink} className="text-gray-800 hover:underline">
           View
         </a>
-        <a href={editLink} className='text-gray-800 hover:underline'>
+        <a href={editLink} className="text-gray-800 hover:underline">
           Edit
         </a>
       </td>
