04e5189ecc7a79a6622e74cbcdf493e8f73121f0
Thomas Kam <thomassth@users.noreply.github.com>
2025-07-05 02:46:43 -0400

    config

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -14,6 +14,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
-  staticDirs: ["../public", "../static"],
+  staticDirs: ["../public"],
 };
 export default config;
