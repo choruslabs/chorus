e409ab5742d97a8227358152cfa0f6151cca32b6
Maanas Arora <maanasarora23@gmail.com>
2026-01-07 23:43:09 -0500

    refactor: remove unused imports and comment out logo component

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -1,15 +1,15 @@
 // TODO: design an actual logo!
 
-const Logo = ({ fill }: { fill?: string }) => (
-  <svg
-    width="20"
-    viewBox="0 0 88 100"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <title>Logo</title>
-    <circle cx="44" cy="50" r="15" fill={fill ?? "black"} />
-  </svg>
-);
+// const Logo = ({ fill }: { fill?: string }) => (
+//   <svg
+//     width="20"
+//     viewBox="0 0 88 100"
+//     fill="none"
+//     xmlns="http://www.w3.org/2000/svg"
+//   >
+//     <title>Logo</title>
+//     <circle cx="44" cy="50" r="15" fill={fill ?? "black"} />
+//   </svg>
+// );
 
-export default Logo;
+// export default Logo;
