55e46460880933735a760dfa9947e32e2805ed7d
Maanas Arora <maanasarora23@gmail.com>
2026-04-27 04:00:12 -0400

    Remove unused import of useEffect from ParticipationSpa component

--- client/src/components/participation/ParticipationSpa.tsx
+++ client/src/components/participation/ParticipationSpa.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import CoreBase from '../../app/core/base';
 import type { ConversationCustomization } from '../../app/core/conversation';
 import type {
